11484d9caa330021a3106844a304f32cc1c2a409
dpldgr <144766863+dpldgr@users.noreply.github.com>
2026-06-16 11:47:19 +0800

    Support matching-width board presets

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,11 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <cstdlib>
 #include <iostream>
 
 #include "bitboard.h"
+#include "misc.h"
 #include "endgame.h"
 #include "position.h"
 #include "psqt.h"
@@ -35,16 +37,33 @@
 
 using namespace Stockfish;
 
+namespace {
+
+void startup_telemetry(const char* message) {
+
+  if (std::getenv("FSF_INIT_TELEMETRY"))
+      sync_cout << "info string " << message << sync_endl;
+}
+
+} // namespace
+
 int main(int argc, char* argv[]) {
 
   std::cout << engine_info() << std::endl;
 
+  startup_telemetry("pieceMap.init(): entered");
   pieceMap.init();
+  startup_telemetry("variants.init(): entered");
   variants.init();
+  startup_telemetry("CommandLine::init(): entered");
   CommandLine::init(argc, argv);
+  startup_telemetry("UCI::init(): entered");
   UCI::init(Options);
+  startup_telemetry("Tune::init(): entered");
   Tune::init();
+  startup_telemetry("PSQT::init(): entered");
   PSQT::init(variants.find(Options["UCI_Variant"])->second);
+  startup_telemetry("main(): before Bitboards::init()");
   Bitboards::init();
   Position::init();
   Bitbases::init();
--- src/position.cpp
+++ src/position.cpp
@@ -161,7 +161,7 @@ void Position::init() {
 
   for (Color c : {WHITE, BLACK})
       for (PieceType pt = PAWN; pt <= KING; ++pt)
-          for (Square s = SQ_A1; s <= SQ_MAX; ++s)
+          for (Square s = SQ_MIN; s <= SQ_MAX; ++s)
               Zobrist::psq[make_piece(c, pt)][s] = rng.rand<Key>();
 
   for (File f = FILE_A; f <= FILE_MAX; ++f)
@@ -182,7 +182,7 @@ void Position::init() {
           for (int n = 0; n < SQUARE_NB; ++n)
               Zobrist::inHand[make_piece(c, pt)][n] = rng.rand<Key>();
 
-  for (Square s = SQ_A1; s <= SQ_MAX; ++s)
+  for (Square s = SQ_MIN; s <= SQ_MAX; ++s)
       Zobrist::wall[s] = rng.rand<Key>();
 
   for (int i = NO_EG_EVAL; i < EG_EVAL_NB; ++i)
@@ -196,7 +196,7 @@ void Position::init() {
       for (PieceSet ps = CHESS_PIECES & ~piece_set(PAWN); ps;)
       {
       Piece pc = make_piece(c, pop_lsb(ps));
-      for (Square s1 = SQ_A1; s1 <= SQ_MAX; ++s1)
+      for (Square s1 = SQ_MIN; s1 <= SQ_MAX; ++s1)
           for (Square s2 = Square(s1 + 1); s2 <= SQ_MAX; ++s2)
               if ((type_of(pc) != PAWN) && (attacks_bb(c, type_of(pc), s1, 0) & s2))
               {
